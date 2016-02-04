9fb7bf84e4d5cac4f6096c17c9c40a0e40046074
dantran <dantrandinhh@gmail.com>
2016-02-04 14:50:39 +0700

    publish1

---
+++ Assets/Script/MoveBall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBall : MonoBehaviour {
+
+    private Rigidbody rigid;
+    private float moveSpeed;
+	// Use this for initialization
+	void Start () {
+        moveSpeed = 5.0f;
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	
+    
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rigid.AddForce(movement*moveSpeed);
+        Debug.Log("Move!");
+    }
+}
